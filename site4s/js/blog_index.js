b071525313862ae3a7cdc1a1e891465086b2f48f
nikitave <66897293+nikitave@users.noreply.github.com>
2020-11-18 19:54:31 +0700

    added

---
+++ site4s/js/blog_index.js
@@ -0,0 +1,11 @@
+
+$(function () {
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    navToggle.on("click", function(event){
+        event.preventDefault();
+        nav.toggleClass("show");
+    });
+
+});
